603bae9dbf60aea3981916553fdf05a406d8f195
Kasperejros <krajewski.kacper@gmail.com>
2018-09-16 22:26:51 +0200

    initial

---
+++ desktop/src/com/upgrade/sezonowiec/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.upgrade.sezonowiec.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.upgrade.sezonowiec.mainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new mainGame(), config);
+	}
+}
